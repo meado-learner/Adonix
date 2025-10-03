4733e1fbedac6006957b7f4bc74b20a4fdeab452
meado-learner <meadonix@gmail.com>
2025-10-03 14:13:51 -0600

    Update _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -2,20 +2,13 @@ import fetch from 'node-fetch'
 
 export async function before(m, { conn }) {
 
-  const canales = [
-    { id: global.id_canal, name: global.name_canal },
-    { id: global.id_canal2, name: global.name_canal2 }
-  ]
-
-  const canalSeleccionado = canales[Math.floor(Math.random() * canales.length)]
-
   global.rcanal = {
     contextInfo: {
       isForwarded: true,
       forwardedNewsletterMessageInfo: {
-        newsletterJid: canalSeleccionado.id,
+        newsletterJid: id_canal,
         serverMessageId: 100,
-        newsletterName: canalSeleccionado.name,
+        newsletterName: name_canal,
       }
     }
   }
