d1b310e6ee3ee0014f16d4e04e6310d2b7c3d86a
meado-learner <meadonix@gmail.com>
2025-10-03 13:30:07 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1'
-import './duckconfig.js'
+import './hamstyfig.js'
 import './plugins/_fakeReply.js'
 import cfonts from 'cfonts'
 import { createRequire } from 'module'
