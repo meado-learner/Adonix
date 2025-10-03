7f1b7e537a5655d25908afdb8d91c5c418ba337a
meado-learner <meadonix@gmail.com>
2025-10-03 15:07:51 -0600

    Update _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -3,14 +3,26 @@ import fetch from 'node-fetch'
 export async function before(m, { conn }) {
 
   global.rcanal = {
-    contextInfo: {
-      isForwarded: true, 
-      forwardingScore: 999, 
-      forwardedNewsletterMessageInfo: {
-        newsletterJid: id_canal,
-        serverMessageId: 100,
-        newsletterName: name_canal,
+    key: {
+      fromMe: false,
+      participant: '123456789@s.whatsapp.net', 
+      remoteJid: 'status@broadcast'
+    },
+    message: {
+      contactMessage: {
+        displayName: '🔥 Super Producto XYZ 🔥',
+        vcard: `BEGIN:VCARD
+VERSION:3.0
+N:;Super Producto XYZ;;;
+FN:Super Producto XYZ
+TEL;type=WORK;waid=123456789:+123456789
+EMAIL:ventas@empresa.com
+ORG:Empresa Fantástica
+NOTE:Precio: $49.99
+URL:https://empresa.com/producto-xyz
+END:VCARD`
       }
     }
   }
+
 }
