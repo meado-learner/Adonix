9044f98d67c69b89041c6c610f303861baae5443
meado-learner <meadonix@gmail.com>
2025-10-04 15:03:43 -0600

    Update handler.js

--- handler.js
+++ handler.js
@@ -392,7 +392,7 @@ admin: `✦ El comando *${comando}* solo puede ser usado por los administradores
 botAdmin: `✦ Para ejecutar el comando *${comando}* debo ser administrador del grupo.`,
 restrict: `✦ Esta caracteristica está desactivada.`
 }[type]
-if (msg) return conn.reply(m.chat, msg, m, rcanal).then(_ => m.react('✖️'))
+if (msg) return conn.reply(m.chat, msg, m).then(_ => m.react('✖️'))
 }
 let file = global.__filename(import.meta.url, true)
 watchFile(file, async () => {
