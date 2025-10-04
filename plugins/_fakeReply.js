55e29b35e079aa8c1fa3ce9e20d52762e3b59aeb
meado-learner <meadonix@gmail.com>
2025-10-04 08:20:51 -0600

    Update _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -3,25 +3,15 @@ import fetch from 'node-fetch'
 export async function before(m, { conn }) {
 
   global.rcanal = {
-    key: {
-      fromMe: false,
-      participant: '123456789@s.whatsapp.net', 
-      remoteJid: 'status@broadcast'
-    },
-    message: {
-      contactMessage: {
-        displayName: '🔥 Super Producto XYZ 🔥',
-        vcard: `BEGIN:VCARD
-VERSION:3.0
-N:;Super Producto XYZ;;;
-FN:Super Producto XYZ
-TEL;type=WORK;waid=123456789:+123456789
-EMAIL:ventas@empresa.com
-ORG:Empresa Fantástica
-NOTE:Precio: $49.99
-URL:https://empresa.com/producto-xyz
-END:VCARD`
-      }
+    externalAdReply: {
+      title: botname,
+      body: author,
+      mediaUrl: 'https://apiadonix.kozow.com',
+      previewType: "PHOTO",
+      thumbnail: await (await fetch(banner)).buffer(),
+      sourceUrl: 'https://apiadonix.kozow.com',
+      mediaType: 1,
+      renderLargerThumbnail: false
     }
   }
 
