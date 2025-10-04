bce1ef7abc7b7cdb94d3a436fbe5562a4f429b60
meado-learner <meadonix@gmail.com>
2025-10-04 14:35:13 -0600

    Actualizar grupo-abrir_cerrar.js

--- plugins/grupo-abrir_cerrar.js
+++ plugins/grupo-abrir_cerrar.js
@@ -1,8 +1,10 @@
 import { areJidsSameUser } from '@whiskeysockets/baileys'
 
 const handler = async (m, { conn, command, participants, groupMetadata, isAdmin, isOwner }) => {
-  if (!m.isGroup) return m.reply('「✦」 Este comando solo puede usarse en grupos.')
-  if (!isAdmin && !isOwner) return m.reply('「✦」 Solo un administrador puede usar este comando.')
+  if (!m.isGroup)
+    return conn.sendMessage(m.chat, { text: '「✦」 Este comando solo puede usarse en grupos.' }, { quoted: fkontak })
+  if (!isAdmin && !isOwner)
+    return conn.sendMessage(m.chat, { text: '「✦」 Solo un administrador puede usar este comando.' }, { quoted: fkontak })
 
   const group = groupMetadata.id
   const isClose = groupMetadata.announce === true
@@ -18,23 +20,27 @@ const handler = async (m, { conn, command, participants, groupMetadata, isAdmin,
   }
 
   if (command === 'abrir') {
-    if (!isClose) return m.reply('「✦」El grupo ya está abierto.')
+    if (!isClose)
+      return conn.sendMessage(m.chat, { text: '「✦」 El grupo ya está abierto.' }, { quoted: fkontak })
+
     await conn.groupSettingUpdate(group, 'not_announcement')
-    const text = `「✦」 *El grupo ha sido abierto todos pueden escribir nuevamente.*`
+    const text = `「✦」 *El grupo ha sido abierto, todos pueden escribir nuevamente.*`
     await conn.sendMessage(group, { text, mentions: participants.map(v => v.id) }, { quoted: fkontak })
   }
 
   if (command === 'cerrar') {
-    if (isClose) return m.reply('「✦」 El grupo ya se encuentra cerrado.')
+    if (isClose)
+      return conn.sendMessage(m.chat, { text: '「✦」 El grupo ya se encuentra cerrado.' }, { quoted: fkontak })
+
     await conn.groupSettingUpdate(group, 'announcement')
-    const text = `「✦」 *El grupo ha sido cerrado temporalmente..*`
+    const text = `「✦」 *El grupo ha sido cerrado temporalmente.*`
     await conn.sendMessage(group, { text, mentions: participants.map(v => v.id) }, { quoted: fkontak })
   }
 }
 
 handler.help = ['abrir', 'cerrar']
 handler.tags = ['grupo']
-handler.command = ['abrir', 'cerrar']
+handler.command = /^(abrir|cerrar)$/i
 handler.admin = true
 handler.group = true
 
