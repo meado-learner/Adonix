8c0a569ce0e7e336fbfbf2719213db571d826e62
meado-learner <meadonix@gmail.com>
2025-10-07 08:00:12 -0600

    Update sockets-serbot.js

--- features/sockets-serbot.js
+++ features/sockets-serbot.js
@@ -205,10 +205,10 @@ if (i < 0) return
 delete global.conns[i]
 global.conns.splice(i, 1)
 }}, 60000)
-let handler = await import('../handler.js')
+let handler = await import('../events.js')
 let creloadHandler = async function (restatConn) {
 try {
-const Handler = await import(`../handler.js?update=${Date.now()}`).catch(console.error)
+const Handler = await import(`../events.js?update=${Date.now()}`).catch(console.error)
 if (Object.keys(Handler || {}).length) handler = Handler
 } catch (e) {
 console.error('⚠︎ Nuevo error: ', e)
